89d1b8450cae14d8c891d179103c82d12d3708ae
Yari Cherchi <yari.cherchi7@gmail.com>
2022-12-22 17:11:19 +0100

    es-6 svolto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HelloWord } from "./HelloWord";
+
+
+
+export class App extends React.Component {
+    render(){
+        return <div> 
+            <HelloWord/>
+        </div>
+    }
+}
