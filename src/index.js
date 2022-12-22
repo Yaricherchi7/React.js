0023a55fb644f1bff865d4568c510adfdcfe79db
Yari Cherchi <yari.cherchi7@gmail.com>
2022-12-22 17:13:50 +0100

    integrato il component nel root

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelloWord } from './HelloWord';
+import { App } from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelloWord/>
+    <App/>
   </React.StrictMode>
 );
 
