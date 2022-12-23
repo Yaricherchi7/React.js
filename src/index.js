403d08302d2325654d332be4c34d84a29016ffab
Yari Cherchi <yari.cherchi7@gmail.com>
2022-12-23 11:56:32 +0100

    modify es-7

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HelloWord } from './HelloWord';
 import { App } from './App';
 
 
@@ -8,7 +7,6 @@ import { App } from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWord/>
     <App/> 
   </React.StrictMode>
 );
