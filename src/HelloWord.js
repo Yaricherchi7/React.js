f0f8f815b25e639e013bdccfded168fcc7e87bd9
Yari Cherchi <yari.cherchi7@gmail.com>
2022-12-23 11:53:43 +0100

    es-7 svolto

--- src/HelloWord.js
+++ src/HelloWord.js
@@ -2,6 +2,11 @@ import React from "react"
 
 export class HelloWord extends React.Component {
     render() {
-        return <h1>Hello Word!</h1>
+        return (
+            <div>
+              <h1>Hello Word!</h1>
+              <p>What a beautiful day!</p>
+            </div>
+          );
     }
 }
