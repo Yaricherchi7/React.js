bb0965e5bd447cc7cb0e9856d07d279e4f5ade20
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-05 15:02:14 +0100

    es svolto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { HelloWord } from "./HelloWord";
-import { Welcome } from "./welcome";
-
+import { Welcome } from "./Welcome";
 
 
 export class App extends React.Component {
     render(){
         return <div> 
             <HelloWord/>
-            <Welcome name="Yari"/>
+            <Welcome />
         </div>
     }
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -6,3 +6,8 @@ export class Welcome extends React.Component {
         return <p> Welcome {this.props.name}</p>
     }
 }
+
+
+Welcome.defaultProps = {
+    name: "Yari"
+  }
