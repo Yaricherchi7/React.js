54d6ee13cd46125d2426c5e0797f191d21092cbd
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-05 15:55:24 +0100

    es-13 svolto

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ export class Welcome extends React.Component {
     render(){
         return <div>
         <p> Welcome {this.props.name}!</p>
-        <p> Your age is : {this.props.age}!</p>
+        <p> Your age is : <strong> {this.props.age}!</strong></p>
         </div>
     }
 }
