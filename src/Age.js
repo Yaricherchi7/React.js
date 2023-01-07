4bb5516f1d815f7890c34f816e5f0545a347a26b
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-07 12:56:37 +0100

    es-19 svolto

--- src/Age.js
+++ src/Age.js
@@ -4,6 +4,9 @@ import React from "react";
 
 export  class Age extends React.Component {
     render(){
-        return <p>Your age is: </p>
+        return <div>
+            {this.props.age > 18 ? <p>your age is: {this.props.age} </p> : <p>You are very young!</p>}
+        </div>
+        
     }
 }
