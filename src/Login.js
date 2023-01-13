0676f50528238a6bfd2a0506675d12ebb035809b
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-13 10:53:45 +0100

    es 27 svolto

--- src/Login.js
+++ src/Login.js
@@ -1,59 +1,66 @@
 import React from "react";
 
-
 export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    checkbox: "",
+    onLogin: "false"
+  };
 
-    state = {
-        username : "",
-        password : "",
-        checkbox: "",
-    }
-
-    HandlerInputchange = (event) =>{
-        const value = event.target.value
-        const name = event.target.name
-        const type = event.target.type
-        const checked = event.target.checked
+  HandlerInputchange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    const type = event.target.type;
+    const checked = event.target.checked;
 
         this.setState({
-            [name]: type === "checkbox" ? checked : value
-            
-        })
+        [name]: type === "checkbox" ? checked : value,
+        
+        });
     }
 
-render(){
-    return(
+    HandlerClick = ()=> {
+        this.setState({
+            onLogin : true,
+        })
+    }
+    componentDidUpdate(){
+        console.log(this.state)
+    }
+  
+
+  render() {
+        return (
         <div>
-            <input 
+            <input
             name="username"
-            type="username" 
+            type="username"
             value={this.state.value}
             onChange={this.HandlerInputchange}
             />
-            
-            <input 
+
+            <input
             name="password"
-            type="password" 
+            type="password"
             value={this.state.password}
             onChange={this.HandlerInputchange}
             />
 
-<input 
+            <input
             name="checkbox"
-            type="checkbox" 
+            type="checkbox"
             value={this.state.password}
             onChange={this.HandlerInputchange}
             />
+            <button 
+            disabled={!this.state.username || !this.state.password}
+            onClick={this.HandlerClick}
+            >
+            Bottone
+            </button>
         </div>
-    )
+        );
+    }
 }
-}
 
-
-
-
-
-
-/* Create a Login component containing three inputs: a username input,
- a password input and a remember checkbox.
-  All three inputs should be controlled components. */
