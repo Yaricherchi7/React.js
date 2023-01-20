48f15addc78742139173da84bf6551429bbf5b6a
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-20 10:24:28 +0100

    es svolto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { TodoList } from "./TodoList";
 
 import { UncontrolledLogin } from "./UncontrolledLogin";
+import { Welcome } from "./Welcome";
 
 
 
@@ -9,6 +10,7 @@ export class App extends React.Component {
     render(){
         return (
         <div> 
+            <Welcome className="welcome" name="Jhon" age="22" />
             <h1> My First from</h1>
             <UncontrolledLogin/>
             <TodoList/>
