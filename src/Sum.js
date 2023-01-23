a2a61147f7e18dc6a61a8572f72667166afaa505
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-23 16:11:58 +0100

    es svolto

--- src/Sum.js
+++ src/Sum.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export function Sum({number = [1,2,3,4,5,6,7,8,9,10]}){
-     /* const sum = {number} */
+     
+    /* in questo caso ho impostato un valore predefinito a "number" 
+    inserendolo direttamente nel props della funzione */
 
     return (
         <div>
