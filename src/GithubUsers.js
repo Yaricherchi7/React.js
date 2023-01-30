248f6c83175d62a32b11cb66b7a29f5a4e142295
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-30 16:44:31 +0100

    es svolto

--- src/GithubUsers.js
+++ src/GithubUsers.js
@@ -13,7 +13,7 @@ export function GithubUsers({username}){
             <button className=" border-2 border-black mt-2" onClick={HandleGetUserData}> load user data</button>
             {loading && <h1>loading...</h1>}
             {data && <h1>{data.name}</h1>}
-            {error && <h1> there as been an error..</h1>}
+            {error && <h1> there as been an error...</h1>}
             {data && <img src={data.avatar_url} alt={data.name}/>}
             
         </div>
