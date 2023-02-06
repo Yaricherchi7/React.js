3755bd07192eb7ae657f198141d4e5be14ebc7cd
Yari Cherchi <yari.cherchi7@gmail.com>
2023-02-06 16:06:14 +0100

    es svolto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, } from "react-router-dom";
 import {Welcome} from "./Welcome"
 import { HookCounter } from "./HookCounter";
 import { ShowGithubUser } from "./ShowGithubUser";
+import { Link } from "react-router-dom";
 
 
 
@@ -9,6 +10,11 @@ import { ShowGithubUser } from "./ShowGithubUser";
 export function App() {
     return (
         <div>
+          <div className="flex justify-center gap-2">
+                <Link className="m-2 p-2 border-2 border-black " to='/'>Welcome</Link>
+                <Link className=" m-2 p-2 border-2 border-black " to='/counter'>Counter</Link >
+                <Link className="m-2 p-2 border-2 border-black " to='/users/Yaricherchi7'>Github Profile</Link>
+          </div>
           <Routes>
              <Route path="/" element={<Welcome/>}/>
              <Route path="/counter" element={<HookCounter/>}/>
